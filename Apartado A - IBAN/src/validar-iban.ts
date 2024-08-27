e4a9685284095158b7a3a037392a58f5c585fd44
Guste Gaubaite <219.guste@gmail.com>
2024-08-27 13:11:16 +0200

    Obtener input del IBAN

--- Apartado A - IBAN/src/validar-iban.ts
+++ Apartado A - IBAN/src/validar-iban.ts
@@ -1,1 +1,16 @@
 import "./style.css";
+
+const botonValidar = document.querySelector(".validar");
+
+const obtenerInput = () => {
+  let textoInput = "";
+  const inputValidar = document.getElementById("validar");
+  if (inputValidar && inputValidar instanceof HTMLInputElement) {
+    textoInput = inputValidar.value;
+  }
+  return textoInput;
+};
+
+if (botonValidar && botonValidar instanceof HTMLButtonElement) {
+  botonValidar.addEventListener("click", obtenerInput);
+}
