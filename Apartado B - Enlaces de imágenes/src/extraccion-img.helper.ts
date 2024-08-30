918e4eb3775f75052a13802931df2dbe70f8135c
Guste Gaubaite <219.guste@gmail.com>
2024-08-30 10:04:42 +0200

    Implementada función que extrae los enlaces del texto

--- Apartado B - Enlaces de imágenes/src/extraccion-img.helper.ts
+++ Apartado B - Enlaces de imágenes/src/extraccion-img.helper.ts
@@ -6,3 +6,13 @@ export const obtenerInput = () => {
   }
   return texto;
 };
+
+export const extraerEnlacesImagenes = (texto: string) => {
+  let urlsImagenes = [];
+  const patron = /<img[^>]*src="([^"]*)"[^>]*>/g;
+  let coincidencia;
+  while ((coincidencia = patron.exec(texto)) !== null) {
+    urlsImagenes.push(coincidencia[1]);
+  }
+  return urlsImagenes;
+};
--- Apartado B - Enlaces de imágenes/src/extraccion-img.ts
+++ Apartado B - Enlaces de imágenes/src/extraccion-img.ts
@@ -1,1 +1,13 @@
+import { extraerEnlacesImagenes, obtenerInput } from "./extraccion-img.helper";
 import "./style.css";
+
+const botonExtraerImagenes = document.querySelector(".extraer-imagenes");
+
+const extraerImagenes = () => {
+  const texto = obtenerInput();
+  extraerEnlacesImagenes(texto);
+};
+
+if (botonExtraerImagenes && botonExtraerImagenes instanceof HTMLButtonElement) {
+  botonExtraerImagenes.addEventListener("click", extraerImagenes);
+}
